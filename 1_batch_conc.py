0a2bfec1856cee93f12829c6659048a917193d39
nicolas <nicolas.ambert@gmail.com>
2017-02-23 11:39:46 +0100

    SimulatoreCore_1 OdeSystems_1 SaveData_1 pour lancer sur queue

--- 1_batch_conc.py
+++ 1_batch_conc.py
@@ -16,7 +16,7 @@ import shutil
 
 # Ligand
 
-values = np.logspace(-6,1,50)
+values = np.logspace(-6,1,20)
 
 outDirectory = "DRC_ACh"
 
@@ -44,20 +44,28 @@ shutil.copyfile(simu_params['inCsteFile'], outDirectory + os.sep+ simu_params['i
 starterBatch = open(outDirectory +os.sep + "start_All_launch.sh",'a')
 
 
-"""
+
 # for cluster mode
 fileLaunchBatch = open(outDirectory +os.sep + "launch_00.sh",'w')
 fileLaunchBatch.write("#!/usr/bin/env bash \n#Job name \n#SBATCH -J ")
 fileLaunchBatch.write("Jobs_00" + " \n")
+
+fileLaunchBatch.write("#SBATCH --mem-per-cpu=5000 \n")
+fileLaunchBatch.write("#SBATCH --ntasks 1 \n")
 fileLaunchBatch.write("#SBATCH --ntasks-per-core=1 \n")
-fileLaunchBatch.write("#SBATCH --mem=500 \n")
+
+fileLaunchBatch.write("#SBATCH --exclusive \n")
+fileLaunchBatch.write("#SBATCH --time=1 \n")
 fileLaunchBatch.write("scontrol show jobid -dd ${SLURM_JOBID} \n")
-fileLaunchBatch.write("time echo $HOSTNAME  sleep 10") 
+fileLaunchBatch.write("srun sleep 10") 
 fileLaunchBatch.close()
 
+#for i in $(ls *.sh); do
+#	echo item: $i
+#done
 starterBatch.write("sbatch "+ "launch_00.sh"+" \n")
-starterBatch.write("sleep 1 \n")
-"""
+starterBatch.write("sleep 5 \n")
+
 
 
 
@@ -83,14 +91,31 @@ for i, iconc in enumerate(values):
 	
 	print outDirectory
 
+	shutil.copyfile(outDirectory + os.sep+ "SimulatoreCore.py", outDirectory + os.sep+ "SimulatoreCore_"+str(i)+".py")
+	shutil.copyfile(outDirectory + os.sep+ "ODESystems.py", outDirectory + os.sep+ "ODESystems_"+str(i)+".py")
+	shutil.copyfile(outDirectory + os.sep+ "SaveData.py", outDirectory + os.sep+ "SaveData_"+str(i)+".py")
+
+#sed -i -e "s/ODESystems/ODESystems_1/g"
+
+	os.system("sed -i -e 's/ODESystems/ODESystems_"+str(i)+"/g' "+ outDirectory + os.sep+ "SimulatoreCore_"+str(i)+".py")
+	os.system("sed -i -e 's/SaveData/SaveData_"+str(i)+"/g' "+ outDirectory + os.sep+ "SimulatoreCore_"+str(i)+".py")
+
+
 	sys.stdout.flush()
 	
 	# for cluster mode
 	fileLaunchBatch = open(outDirectory +os.sep + "launch_"+str(i)+".sh",'w')
 	fileLaunchBatch.write("#!/usr/bin/env bash \n#Job name \n#SBATCH -J ")
 	fileLaunchBatch.write("Jobs_"+str(i) + " \n")
+	fileLaunchBatch.write("#SBATCH --mem-per-cpu=5000 \n")
+	fileLaunchBatch.write("#SBATCH --ntasks 1 \n")
 	fileLaunchBatch.write("#SBATCH --ntasks-per-core=1 \n")
-	fileLaunchBatch.write("#SBATCH --mem=5000 \n")
+
+	fileLaunchBatch.write("#SBATCH --exclusive \n")
+	fileLaunchBatch.write("#SBATCH --time=1 \n")
+
+#SBATCH --qos=lowpri
+#	fileLaunchBatch.write("#SBATCH --ntasks-per-core=1 \n")
 
 	fileLaunchBatch.write("echo 'LOCALID = ' $SLURM_LOCALID \n")
 	fileLaunchBatch.write("echo 'PROCID = ' $SLURM_PROCID \n")
@@ -100,12 +125,13 @@ for i, iconc in enumerate(values):
 #	fileLaunchBatch.write("#SBATCH -e slurm." + str(i) + ".err \n")
 
 	fileLaunchBatch.write("scontrol show jobid -dd ${SLURM_JOBID} \n")
-	fileLaunchBatch.write("time python SimulatoreCore.py " + fPRMS) 
+	fileLaunchBatch.write("srun python SimulatoreCore_"+str(i)+".py " + fPRMS) 
 	fileLaunchBatch.close()
 
+	#os.system("cd "+ outDirectory +os.sep + "; sbatch launch_"+str(i)+".sh")
 
 	starterBatch.write("sbatch "+ "launch_"+str(i)+".sh"+" \n")
-	starterBatch.write("sleep 1 \n")
+	starterBatch.write("sleep 3 \n")
 
 
 	#os.system("cd "+ outDirectory +os.sep + "; sbatch launch_"+str(i)+".sh") 
