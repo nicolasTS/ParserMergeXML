4d7ea4836eb48fb51d6102635c35b0848264d916
nicolasTS <nicolas.ambert@gmail.com>
2017-03-16 11:08:52 +0100

    version parallele avec import pp

--- UTILS/analysis_batch_prms.py
+++ UTILS/analysis_batch_prms.py
@@ -6,17 +6,18 @@ import datetime
 import glob
 import operator
 import numpy as np
+import pp
 
 #################################################
 
 
 namePRMS = sys.argv[1] #"kf_G1"
-#Yfile="IP3"
 Yfile=sys.argv[2] #"Ga_GTP"
+ncpus=int(sys.argv[3])
 
 path = "./batch_"+namePRMS +"/"
 
-Xfile="Cstes_CHO.txt"
+Xfile="Cstes.txt"
 
 #################################################
 
@@ -25,21 +26,50 @@ valueXfile = []
 valueYfile = []
 
 
-# recuperation du prm cinetiq
-for nameX in glob.glob(path +"/*/" + Xfile.rstrip(".txt")+"*"):
-	listDir.append(os.path.dirname(nameX))
-	for i, line in enumerate(file(nameX, 'r' )):
-		(a,b)=line.split(' = ')
-		if (a == " " +namePRMS):
-			valueXfile.append(b[:-1])
+# recupere la valeur du prm cinetique ainsi que le max de l output demande
+def prmMax(cstFile, namePRMS, Yfile):
+	import numpy as np
+	import glob
+	import os
+	for line in file(cstFile, 'r' ):
+		if not line.startswith('#'):
+			(a,b)=line.split(' = ')
+			if (a == " " +namePRMS):
+				valuePrm = float(b[:-1])
 
-
-
-# recuperation du max pour valeur output
-for nameY in listDir:
-	n = glob.glob(nameY + "/*/" + Yfile +"/Sim0.txt")
+	n = glob.glob(os.path.dirname(cstFile) + "/*/" + Yfile +"/Sim0.txt")
 	X, Y = np.loadtxt(n[0],unpack=True)
-	valueYfile.append(max(Y))
+	maxValue = max(Y)
+
+	return valuePrm, maxValue
+
+
+
+ppservers=("*",)
+job_server = pp.Server( ppservers=ppservers, secret='123')
+
+job_server.set_ncpus(ncpus)
+print job_server.get_active_nodes()
+print "Starting ", job_server.get_ncpus(), " workers"
+
+
+jobs = []
+start_time = time.time()
+
+for nameX in glob.glob(path +"/*/" + Xfile.rstrip(".txt")+"*"):
+	#print prmMax(nameX, namePRMS, Yfile)
+	jobs.append(job_server.submit(prmMax,(nameX,namePRMS, Yfile ) ))
+
+
+for job in jobs:
+	a, b = job()
+	valueXfile.append(a)
+	valueYfile.append(b)
+
+
+print "Time elapsed: ", time.time() - start_time, "s"
+job_server.print_stats()
+
 
 data = zip(valueXfile, valueYfile)
 
