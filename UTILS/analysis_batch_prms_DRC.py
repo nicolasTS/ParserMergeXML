839bf4b28aae7106f609452b31d2ebcda0c12962
nicolasTS <nicolas.ambert@gmail.com>
2017-03-20 12:52:56 +0100

    calcul de auc et decay

--- UTILS/analysis_batch_prms_DRC.py
+++ UTILS/analysis_batch_prms_DRC.py
@@ -10,11 +10,13 @@ import pp
 import math, sys, md5, time
 
 
+
+
 #################################################
-path = "batch_kass_re1"
+path = "batch_kass_re16"
 Xfile="Glu"
 Yfile="sumOpen"
-namePRMS = "kass_re1"
+namePRMS = "kass_re16"
 cstesBase = "Cstes"
 ncpus=int(4)
 #################################################
@@ -24,10 +26,14 @@ ncpus=int(4)
 valuePRMfile = []
 valueXfile = []
 valueYfile = []
+valueYfileAUC = []
+valueYfileDecay = []
+
 
 def maxValue3D(fileCste, namePRMS, fileNameX, fileNameY):
 	""" calcule max value for a txt file """
 	import numpy as np
+	from prms_utils import *
 	for line in file(fileCste, 'r' ):
 		if not line.startswith('#'):
 			(a,b)=line.split(' = ')
@@ -36,10 +42,20 @@ def maxValue3D(fileCste, namePRMS, fileNameX, fileNameY):
 
 	X1, Y1 = np.loadtxt(fileNameX,unpack=True)
 	maxDataX = max(Y1)
+
 	X2, Y2 = np.loadtxt(fileNameY,unpack=True)
 	maxDataY = max(Y2)
+	auc=np.trapz(Y2,X2)
 
-	return valuePrm, maxDataX, maxDataY
+	timeForPeak = np.argmax(Y2)
+	dataXcut=X2[timeForPeak:]
+	dataYcut=Y2[timeForPeak:]	    
+	p0 = [maxDataY,max(X2)] # initial guesses
+	pbest = leastsq(residuals,p0,args=(dataYcut,dataXcut),full_output=1)
+	#print 'Decay time fit with mono exp ',pbest[0][1]
+	decay = pbest[0][1]
+	
+	return valuePrm, maxDataX, maxDataY, auc, decay
 
 
 
@@ -72,16 +88,18 @@ for base in glob.glob(path +os.sep + namePRMS +"_*/*/" ):
 		#print fileCste[0]
 		#print "\n"
 		#(a,b,c) = maxValue3D(fileCste[0], namePRMS, item[0], item[1])
+		#print str(a) + " " + str(b) + " " + str(c)		
 		jobs.append(job_server.submit(maxValue3D,(fileCste[0], namePRMS, item[0], item[1] ) ))
-		#print str(a) + " " + str(b) + " " + str(c)
-
+		
 
 for job in jobs:
-	(a, b, c) = job()
+	#valuePrm, maxDataX, maxDataY, auc, decay
+	(a, b, c, d, e) = job()
 	valuePRMfile.append(a)
 	valueXfile.append(b)
 	valueYfile.append(c)
-
+	valueYfileAUC.append(d)
+	valueYfileDecay.append(e)
 
 print "Time elapsed: ", time.time() - start_time, "s"
 job_server.print_stats()
@@ -89,13 +107,13 @@ job_server.print_stats()
 
 
 
-data = zip(valuePRMfile, valueXfile, valueYfile)
+data = zip(valuePRMfile, valueXfile, valueYfile, valueYfileAUC, valueYfileDecay)
 
 
 # sort result file
 numberR={}
 for i in range(len(data)):
-	numberR[i]= [float(data[i][0]),  float(data[i][1]),  float(data[i][2])]
+	numberR[i]= [float(data[i][0]),  float(data[i][1]),  float(data[i][2]), float(data[i][3]), float(data[i][4])]
 # sort according 
 lsrt= sorted(numberR.iteritems(), key=operator.itemgetter(1))
 
@@ -103,9 +121,9 @@ lsrt= sorted(numberR.iteritems(), key=operator.itemgetter(1))
 #ecrire dans un fichier txt
 
 sortFile = file(path + "/analysis_PRMS_" + Xfile + "_" + Yfile + ".txt", 'a')
-print>>sortFile, "#  "+str(namePRMS) + " "+ str(Xfile) + "   " +str(Yfile)
+print>>sortFile, "#  "+str(namePRMS) + " "+ str(Xfile) + "   " +str(Yfile) + str("AUC  Decay")
 for ivalue in lsrt: 
-	print>>sortFile, str(ivalue[1][0]) + " " +str(ivalue[1][1])+ " " +str(ivalue[1][2])
+	print>>sortFile, str(ivalue[1][0]) + " " +str(ivalue[1][1])+ " " +str(ivalue[1][2])+" " +str(ivalue[1][3])+" " +str(ivalue[1][4])
 
 
  
