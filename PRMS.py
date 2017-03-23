dc0cb3157f282e7ebf7fd60fd8062242788c767e
nicolas <nicolas.ambert@gmail.com>
2017-03-23 13:55:47 +0100

    mettre le path pour outDirectory

--- PRMS.py
+++ PRMS.py
@@ -1,18 +1,18 @@
 merge_params = {
-	'listXMLFiles' : ["../M1R_V2.1_Allosteric_site_NAM.xml", "../20101123_Mcurrent_NAM_v11.xml"], 
+	'listXMLFiles' : ["../M1R_V2.1_Allosteric_site_NAM_v2.xml", "../20101123_Mcurrent_NAM_v11.xml"], 
 	'outDirectory' : "./",
 	'outCsteFile' : "./Cstes.txt",
 }
 
 simu_params = {
-	'outDirectory' : "./",
+	'outDirectory' : "/media/nicolas/DATA/ParserMergeXML/",
 	'inCsteFile' : "./Cstes.txt", 
-	'tstop' : 20e3,
+	'tstop' : 30e3,
 	'namePulse' :['ACh', 'Ant', 'L', 'Mod', 'GDP', 'GTP'],
 	'timePulse' : [[3000,4000], [0,0], [0,0], [0,0], [0,0], [0,0]], 
 	'valuePulse' : [1.0,0, 0, 0, 0, 0],
-	'nameSaveY' : ['R','RL', 'RACh', 'RMod', 'RAnt', 'Ga_GTP', 'Ga_GTP_PLC' ,'PIP','PIP2', 'DAG', 'sIP', 'sIP3', 'sIP2', 'sPI'], 
-	'nameSaveAssig' : ['readout_RL','IP', 'IP2', 'IP3'], 
-	'nameSaveEvents' : ['ACh', 'L', 'Mod', 'Ant'],
+	'nameSaveY' : [ 'Ga_GTP' ], 
+	'nameSaveAssig' : ['IP3'], 
+	'nameSaveEvents' : ['ACh'],
 }
 
